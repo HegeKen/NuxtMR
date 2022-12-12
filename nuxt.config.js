3655ff8053ced1b18913c0d3ab67e1ad7a6be341
Hege <hegeken@foxmail.com>
2022-12-13 01:52:22 +0800

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,6 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-  ssr: true,
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'MIUIROMS',
