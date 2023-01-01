d7490616539dd50f943a452dabfa3140c3741fa5
Hege <hegeken@foxmail.com>
2023-01-02 00:46:33 +0800

    Fixing 12C

---
+++ dist/_nuxt/static/1672591483/mobile/en-US/recovery/oxygen/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1672591483",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmobile\u002Fen-US\u002Frecovery\u002Foxygen",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
