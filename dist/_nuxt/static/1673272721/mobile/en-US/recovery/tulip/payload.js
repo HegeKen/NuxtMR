884ef3754205bc0e3b5a648435483238de85d5ae
Hege <hegeken@foxmail.com>
2023-01-10 03:12:11 +0800

    202301W2-Day2

--- dist/_nuxt/static/1673272721/mobile/en-US/recovery/tulip/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/mobile/en-US/recovery/tulip", (function(a){return {data:[{}],fetch:{NaN:{_error:{message:"request to https:\u002F\u002Fdata.miuier.com\u002Fdata\u002Fdevices\u002Ftulip.json failed, reason: connect ETIMEDOUT 34.124.186.36:443",type:"system",errno:a,code:a,statusCode:500}}},mutations:void 0}}("ETIMEDOUT")));
