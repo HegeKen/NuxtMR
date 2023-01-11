2635a494676c2ad73a1d5c32db13be0719e7201a
Hege <hegeken@foxmail.com>
2023-01-11 15:59:28 +0800

    202301W2-Day3

--- dist/_nuxt/static/1673375178/mobile/zh-CN/recovery/rembrandt/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1673375178",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmobile\u002Fzh-CN\u002Frecovery\u002Frembrandt",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
