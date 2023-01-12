bf0bfa69561ecc7ad54057d2d7decd55397aa4b4
Hege <hegeken@foxmail.com>
2023-01-13 06:55:12 +0800

    202301W2-Day5

--- dist/_nuxt/static/1673532397/mobile/zh-CN/devices/ice/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/mobile/zh-CN/devices/ice", (function(a){return {data:[{}],fetch:{NaN:{_error:{message:"request to https:\u002F\u002Fdata.miuier.com\u002Fdata\u002Fdevices\u002Fice.json failed, reason: read ECONNRESET",type:"system",errno:a,code:a,statusCode:500}}},mutations:void 0}}("ECONNRESET")));
--- dist/_nuxt/static/1673532397/mobile/zh-CN/recovery/nitrogen/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1673532397",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmobile\u002Fzh-CN\u002Frecovery\u002Fnitrogen",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
