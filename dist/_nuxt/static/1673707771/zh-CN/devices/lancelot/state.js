ae21199a37abab203d1294371575e755c6b43589
Hege <hegeken@foxmail.com>
2023-01-14 22:59:18 +0800

    202301W2-Day6

--- dist/_nuxt/static/1673707771/zh-CN/devices/lancelot/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1673707771",layout:"default",error:a,serverRendered:true,routePath:"\u002Fzh-CN\u002Fdevices\u002Flancelot",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,13 @@
 export default {
   head() {
     return {
-      title: 'MIUI官方ROM仓库 - MIUI Official ROMS'
+      title: 'MIUI官方ROM仓库 - MIUI Official ROMS',
+      meta:[
+        {
+          'http-equiv':"refresh",
+          content:"0;url= https://roms.miuier.com/"
+        }
+      ]
     }
   }
 }
