ed3443ea8c2a030c832befea5b9d502906e1fce0
Hege <hegeken@foxmail.com>
2023-01-19 20:24:43 +0800

    20230119-01W3D4

--- dist/_nuxt/static/1674120118/mobile/zh-CN/recovery/land/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1674120118",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmobile\u002Fzh-CN\u002Frecovery\u002Fland",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
