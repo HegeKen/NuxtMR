017ba24883a5e16775850f8a361ff60b815e66f0
Hege <hegeken@foxmail.com>
2023-01-20 15:33:41 +0800

    20230120-01W3D5

--- dist/_nuxt/static/1674130945/en-US/devices/cupid/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1674130945",layout:"default",error:a,serverRendered:true,routePath:"\u002Fen-US\u002Fdevices\u002Fcupid",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
--- dist/_nuxt/static/1674130945/en-US/weekly/week-582-cn/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1674130945",layout:"default",error:a,serverRendered:true,routePath:"\u002Fen-US\u002Fweekly\u002Fweek-582-cn",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
