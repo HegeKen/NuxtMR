2ee286e0f9a130fb525434ab3b46c7ba714de95a
Hege <hegeken@foxmail.com>
2023-01-21 22:43:26 +0800

    20230121-01W3D6

---
+++ dist/_nuxt/static/1674311858/en-US/devices/picasso_48m/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1674311858",layout:"default",error:a,serverRendered:true,routePath:"\u002Fen-US\u002Fdevices\u002Fpicasso_48m",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ dist/_nuxt/static/1674311858/zh-CN/devices/haydn/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/zh-CN/devices/haydn", (function(a){return {data:[{}],fetch:{"0":{_error:{message:"request to https:\u002F\u002Fdata.miuier.com\u002Fdata\u002Fdevices\u002Fhaydn.json failed, reason: read ECONNRESET",type:"system",errno:a,code:a,statusCode:500}}},mutations:void 0}}("ECONNRESET")));
