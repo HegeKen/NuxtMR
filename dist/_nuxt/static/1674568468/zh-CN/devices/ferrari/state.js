b19f675e2dc2d0ab88701d259f94c3364382926d
Hege <hegeken@foxmail.com>
2023-01-24 21:55:26 +0800

    20230124-M01W4D2

---
+++ dist/_nuxt/static/1674568468/zh-CN/devices/ferrari/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1674568468",layout:"default",error:a,serverRendered:true,routePath:"\u002Fzh-CN\u002Fdevices\u002Fferrari",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
