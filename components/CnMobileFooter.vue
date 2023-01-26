9e7c566785518b757acab2eb2135fdeb0d68e622
Hege <hegeken@foxmail.com>
2023-01-26 11:03:48 +0800

    20230126-M01W4D4

--- components/CnMobileFooter.vue
+++ components/CnMobileFooter.vue
@@ -9,7 +9,7 @@
         <a href="/mobile/zh-CN/" class="mdui-ripple"><i class="mdui-icon material-icons">home</i><label>首页</label></a>
         <a href="/mobile/zh-CN/weekly/" class="mdui-ripple"><i class="mdui-icon material-icons">developer_mode</i><label>橙色星期五</label></a>
         <a href="/mobile/zh-CN/devices/" class="mdui-ripple"><i class="mdui-icon material-icons">devices</i><label>机型列表</label></a>
-        <a href="/mobile/zh-CN/tools" class="mdui-ripple"><i class="mdui-icon material-icons">computer</i><label>刷机工具</label></a>
+        <a href="/mobile/zh-CN/mitools" class="mdui-ripple"><i class="mdui-icon material-icons">computer</i><label>刷机工具</label></a>
         <a href="https://files.miuier.com/recovery/" class="mdui-ripple"><i class="mdui-icon material-icons">usb</i><label>Recovery</label></a>
         <a href="#top"><i class="mdui-icon material-icons">arrow_upward</i><label>返回顶部</label></a>
       </div>
--- components/EnMobileFooter.vue
+++ components/EnMobileFooter.vue
@@ -9,7 +9,7 @@
         <a href="/mobile/en-US/" class="mdui-ripple"><i class="mdui-icon material-icons">home</i><label>Home</label></a>
         <a href="/mobile/en-US/weekly/" class="mdui-ripple"><i class="mdui-icon material-icons">developer_mode</i><label>Changelogs</label></a>
         <a href="/mobile/en-US/devices/" class="mdui-ripple"><i class="mdui-icon material-icons">devices</i><label>Devices</label></a>
-        <a href="/mobile/en-US/tools" class="mdui-ripple"><i class="mdui-icon material-icons">computer</i><label>Tools</label></a>
+        <a href="/mobile/en-US/mitools" class="mdui-ripple"><i class="mdui-icon material-icons">computer</i><label>Tools</label></a>
         <a href="https://files.miuier.com/recovery/" class="mdui-ripple"><i class="mdui-icon material-icons">usb</i><label>Recovery</label></a>
         <a href="#top" class="mdui-ripple"><i class="mdui-icon material-icons">arrow_upward</i><label>ToTop</label></a>
       </div>
