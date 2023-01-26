1248f12fa904242016c53cb7d1a0af7ad1798ee4
Hege <hegeken@foxmail.com>
2023-01-26 10:34:47 +0800

    20230126-M01W4D4

---
+++ dist/_nuxt/static/1674700397/mobile/en-US/recovery/mocha/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1674700397",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmobile\u002Fen-US\u002Frecovery\u002Fmocha",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
--- pages/mobile/en-US/tools/index.vue
+++ pages/mobile/en-US/tools/index.vue
@@ -2,95 +2,89 @@
   <div>
     <EnMobileNav />
     <br />
-    <div class="mdui-container-fluid">
-      <div class="mdui-panel mdui-panel-gapless mdui-shadow-0" mdui-panel="">
-        <div class="mdui-panel-item mdui-panel-item-open">
-          <div class="mdui-panel-item-header">
-            <div class="mdui-panel-item-title">MiFlash</div>
-            <i class="mdui-panel-item-arrow mdui-icon material-icons">keyboard_arrow_down</i>
-          </div>
-          <div class="mdui-panel-item-body">
-            <div v-for="(data) in tools.miflash">
-              <p><b>MiFlash Version：</b>{{ data.version }}</p>
-              <p><b>File Name：</b>{{ data.FileName }}</p>
-              <p><b>Download Links：</b>
-                <span v-if="data.url == '' && data.FileName == ''">N/A</span>
-                <span v-else-if="data.url != '' && data.FileName == ''"><a :href="(data.url)">Xiaomi Official</a></span>
-                <span v-else-if="data.url == '' && data.FileName != ''"><a :href="('https://files.miuier.com/tools/MiFlash/' + data.FileName)" class="mdui-text-capitalize">MIUIROMS Files</a></span>
-                <span v-else-if="data.url != '' && data.FileName != ''"><a :href="(data.url)">Xiaomi Official</a> | <a :href="('https://files.miuier.com/tools/MiFlash/' + data.FileName)" class="mdui-text-capitalize">MIUIROMS Files</a></span>
-              </p>
-            </div>
+    <div class="mdui-panel mdui-panel-gapless mdui-shadow-0" mdui-panel="">
+      <div class="mdui-panel-item mdui-panel-item-open">
+        <div class="mdui-panel-item-header">
+          <div class="mdui-panel-item-title mdui-text-color-red">MiFlash</div>
+          <i class="mdui-panel-item-arrow mdui-icon material-icons">keyboard_arrow_down</i>
+        </div>
+        <div class="mdui-panel-item-body">
+          <div v-for="(data) in tools.miflash">
+            <p><b>MiFlash Version：</b>{{ data.version }}</p>
+            <p><b>File Name：</b>{{ data.FileName }}</p>
+            <p><b>Download Links：</b>
+              <span v-if="data.url == '' && data.FileName == ''">N/A</span>
+              <span v-else-if="data.url != '' && data.FileName == ''"><a :href="(data.url)">Xiaomi Official</a></span>
+              <span v-else-if="data.url == '' && data.FileName != ''"><a :href="('https://files.miuier.com/tools/MiFlash/' + data.FileName)" class="mdui-text-capitalize">MIUIROMS Files</a></span>
+              <span v-else-if="data.url != '' && data.FileName != ''"><a :href="(data.url)">Xiaomi Official</a> | <a :href="('https://files.miuier.com/tools/MiFlash/' + data.FileName)" class="mdui-text-capitalize">MIUIROMS Files</a></span>
+            </p>
           </div>
         </div>
       </div>
     </div>
-    <div class="mdui-container-fluid">
-      <div class="mdui-panel mdui-panel-gapless mdui-shadow-0" mdui-panel="">
-        <div class="mdui-panel-item mdui-panel-item-open">
-          <div class="mdui-panel-item-header">
-            <div class="mdui-panel-item-title">Mi PC Suite(English)</div>
-            <i class="mdui-panel-item-arrow mdui-icon material-icons">keyboard_arrow_down</i>
-          </div>
-          <div class="mdui-panel-item-body">
-            <div v-for="(data) in tools.mipcsuite" class="mdui-container-fluid" id="pcsuite">
-              <p><b>Mi PC Suite Version：</b>{{ data.version }}</p>
-              <p><b>Updated at：</b>{{ data.date }}</p>
-              <p><b>File Name：</b>{{ data.FileName }}</p>
-              <p><b>Download Links：</b>
-                <span v-if="data.url == '' && data.FileName == ''">N/A</span>
-                <span v-else-if="data.url != '' && data.FileName == ''"><a :href="(data.url)">Xiaomi Official</a></span>
-                <span v-else-if="data.url == '' && data.FileName != ''"><a :href="('https://files.miuier.com/tools/MiAssistant/' + data.FileName)" class="mdui-text-capitalize">MIUIROMS Files</a></span>
-                <span v-else-if="data.url != '' && data.FileName != ''"><a :href="(data.url)">Xiaomi Official</a> | <a :href="('https://files.miuier.com/tools/MiAssistant/' + data.FileName)" class="mdui-text-capitalize">MIUIROMS Files</a></span>
-              </p>
-              <p v-show="data.showattentions == 'true'">
-                <b>Notes：</b>
-              <ol>
-                <li v-for="(data) in data.attentions" v-show="data.Cn != ''">{{ data.Cn }}</li>
-              </ol>
-              </p>
-              <p v-show="data.showlogs == 'true'">
-                <b>Changelogs：</b>
-              <ol>
-                <li v-for="(data) in data.logs" v-show="data.Cn != ''">{{ data.Cn }}</li>
-              </ol>
-              </p>
-            </div>
+    <div class="mdui-panel mdui-panel-gapless mdui-shadow-0" mdui-panel="">
+      <div class="mdui-panel-item mdui-panel-item-open">
+        <div class="mdui-panel-item-header">
+          <div class="mdui-panel-item-title mdui-text-color-red">Mi PC Suite(English)</div>
+          <i class="mdui-panel-item-arrow mdui-icon material-icons">keyboard_arrow_down</i>
+        </div>
+        <div class="mdui-panel-item-body">
+          <div v-for="(data) in tools.mipcsuite" class="mdui-container-fluid" id="pcsuite">
+            <p><b>Mi PC Suite Version：</b>{{ data.version }}</p>
+            <p><b>Updated at：</b>{{ data.date }}</p>
+            <p><b>File Name：</b>{{ data.FileName }}</p>
+            <p><b>Download Links：</b>
+              <span v-if="data.url == '' && data.FileName == ''">N/A</span>
+              <span v-else-if="data.url != '' && data.FileName == ''"><a :href="(data.url)">Xiaomi Official</a></span>
+              <span v-else-if="data.url == '' && data.FileName != ''"><a :href="('https://files.miuier.com/tools/MiAssistant/' + data.FileName)" class="mdui-text-capitalize">MIUIROMS Files</a></span>
+              <span v-else-if="data.url != '' && data.FileName != ''"><a :href="(data.url)">Xiaomi Official</a> | <a :href="('https://files.miuier.com/tools/MiAssistant/' + data.FileName)" class="mdui-text-capitalize">MIUIROMS Files</a></span>
+            </p>
+            <p v-show="data.showattentions == 'true'">
+              <b>Notes：</b>
+            <ol>
+              <li v-for="(data) in data.attentions" v-show="data.En != ''">{{ data.En }}</li>
+            </ol>
+            </p>
+            <p v-show="data.showlogs == 'true'">
+              <b>Changelogs：</b>
+            <ol>
+              <li v-for="(data) in data.logs" v-show="data.En != ''">{{ data.En }}</li>
+            </ol>
+            </p>
           </div>
         </div>
       </div>
     </div>
-    <div class="mdui-container-fluid">
-      <div class="mdui-panel mdui-panel-gapless mdui-shadow-0" mdui-panel="">
-        <div class="mdui-panel-item mdui-panel-item-open">
-          <div class="mdui-panel-item-header">
-            <div class="mdui-panel-item-title">Mi PC Suite(Chinese)</div>
-            <i class="mdui-panel-item-arrow mdui-icon material-icons">keyboard_arrow_down</i>
-          </div>
-          <div class="mdui-panel-item-body">
-            <div v-for="(data) in tools.miassistant" class="mdui-container-fluid" id="miassistant">
-              <p><b>Mi PC Suite Version：</b>{{ data.version }}</p>
-              <p><b>Updated at：</b>{{ data.date }}</p>
-              <p><b>File Name：</b>{{ data.FileName }}</p>
-              <p><b>Download Links：</b>
-                <span v-if="data.url == '' && data.FileName == ''">N/A</span>
-                <span v-else-if="data.url != '' && data.FileName == ''"><a :href="(data.url)">Xiaomi Official</a></span>
-                <span v-else-if="data.url == '' && data.FileName != ''"><a :href="('https://files.miuier.com/tools/MiAssistant/' + data.FileName)" class="mdui-text-capitalize">MIUIROMS Files</a></span>
-                <span v-else-if="data.url != '' && data.FileName != ''"><a :href="(data.url)">Xiaomi Official</a> | <a :href="('https://files.miuier.com/tools/MiAssistant/' + data.FileName)" class="mdui-text-capitalize">MIUIROMS Files</a></span>
+    <div class="mdui-panel mdui-panel-gapless mdui-shadow-0" mdui-panel="">
+      <div class="mdui-panel-item mdui-panel-item-open">
+        <div class="mdui-panel-item-header">
+          <div class="mdui-panel-item-title mdui-text-color-red">Mi PC Suite(Chinese)</div>
+          <i class="mdui-panel-item-arrow mdui-icon material-icons">keyboard_arrow_down</i>
+        </div>
+        <div class="mdui-panel-item-body">
+          <div v-for="(data) in tools.miassistant" class="mdui-container-fluid" id="miassistant">
+            <p><b>Mi PC Suite Version：</b>{{ data.version }}</p>
+            <p><b>Updated at：</b>{{ data.date }}</p>
+            <p><b>File Name：</b>{{ data.FileName }}</p>
+            <p><b>Download Links：</b>
+              <span v-if="data.url == '' && data.FileName == ''">N/A</span>
+              <span v-else-if="data.url != '' && data.FileName == ''"><a :href="(data.url)">Xiaomi Official</a></span>
+              <span v-else-if="data.url == '' && data.FileName != ''"><a :href="('https://files.miuier.com/tools/MiAssistant/' + data.FileName)" class="mdui-text-capitalize">MIUIROMS Files</a></span>
+              <span v-else-if="data.url != '' && data.FileName != ''"><a :href="(data.url)">Xiaomi Official</a> | <a :href="('https://files.miuier.com/tools/MiAssistant/' + data.FileName)" class="mdui-text-capitalize">MIUIROMS Files</a></span>
 
-              </p>
-              <p v-show="data.showattentions == 'true'">
-                <b>Notes：</b>
-              <ol>
-                <li v-for="(data) in data.attentions" v-show="data.Cn != ''">{{ data.Cn }}</li>
-              </ol>
-              </p>
-              <p v-show="data.showlogs == 'true'">
-                <b>Changelogs：</b>
-              <ol>
-                <li v-for="(data) in data.logs" v-show="data.Cn != ''">{{ data.Cn }}</li>
-              </ol>
-              </p>
-            </div>
+            </p>
+            <p v-show="data.showattentions == 'true'">
+              <b>Notes：</b>
+            <ol>
+              <li v-for="(data) in data.attentions" v-show="data.En != ''">{{ data.En }}</li>
+            </ol>
+            </p>
+            <p v-show="data.showlogs == 'true'">
+              <b>Changelogs：</b>
+            <ol>
+              <li v-for="(data) in data.logs" v-show="data.En != ''">{{ data.En }}</li>
+            </ol>
+            </p>
           </div>
         </div>
       </div>
