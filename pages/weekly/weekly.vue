a38323a7dc63156c26147c48788996a860af7be3
Hege <hegeken@foxmail.com>
2023-02-17 18:01:20 +0800

    Weekly Fix

--- pages/weekly/weekly.vue
+++ pages/weekly/weekly.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
-    <CnPcNav />
+    <CnMobileNav />
     <div class="mdui-container-fluid" v-for="(data) in weeks.versions">
       <div v-if="data.version == 'MIUI 14'">
         <br />
         <div style="padding-left:15px;">{{ data.version }}</div>
         <br />
         <button class="mdui-btn" v-for="(data) in data.weeks">
-          <a :href="('/zh-CN/weekly/week-' + data + '-cn')" v-if="data == weeks.latest"><i class="mdui-icon material-icons">fiber_new</i>第{{ data }}周</a>
-          <a :href="('/zh-CN/weekly/week-' + data + '-cn')" v-else>第{{ data }}周</a>
+          <a :href="('/weekly/logs/week-' + data + '-cn')" v-if="data == weeks.latest"><i class="mdui-icon material-icons">fiber_new</i>第{{ data }}周</a>
+          <a :href="('/weekly/logs/week-' + data + '-cn')" v-else>第{{ data }}周</a>
         </button>
       </div>
 
@@ -17,14 +17,14 @@
         <div style="padding-left:15px;">{{ data.version }}</div>
         <br />
         <button class="mdui-btn" v-for="(data) in data.weeks">
-          <a :href="('/zh-CN/weekly/week-' + data + '-cn')" target="_blank" v-if="data >= weeks.oldest - 1">第{{ data }}周</a>
+          <a :href="('/weekly/logs/week-' + data + '-cn')" target="_blank" v-if="data >= weeks.oldest - 1">第{{ data }}周</a>
           <a :href="('https://old.miuier.com/weekly/logs/week-' + data + '-cn')" target="_blank" v-else>第{{ data }}周</a>
         </button>
       </div>
     </div>
     <br />
-    <CnPcDisclaimer />
-    <CnPcFooter />
+    <CnMobileDisclaimer />
+    <CnMobileFooter />
   </div>
 </template>
 <script>
