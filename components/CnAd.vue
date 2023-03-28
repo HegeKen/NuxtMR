b951e15ba1978b0d2b293d2fe78d6f58efe60eff
Hege <hegeken@foxmail.com>
2023-03-29 02:06:53 +0800

    Add Ads

--- components/CnAd.vue
+++ components/CnAd.vue
@@ -8,22 +8,26 @@
       <div class="mdui-panel-item-body">
         <div><b>进入店铺选购</b></div>
         <br />
-        <div><a href="https://u.jd.com/KQyw7O6">小米手机官方旗舰店</a></div>
+        <div><a href="https://u.jd.com/usHZ8sB">小米手机官方旗舰店</a></div>
+        <br />
+        <div><a href="https://u.jd.com/uqHGd41">小米官方旗舰店</a></div>
         <br />
         <br />
         <div><b>选购手机</b></div>
         <br />
-        <div><a href="https://u.jd.com/Mbl1YWp">在京东选购小米13</a></div>
+        <div><a href="https://u.jd.com/uuHSDCZ">在京东选购小米13</a></div>
         <br />
-        <div><a href="https://u.jd.com/Mzlq2Sd">在京东选购小米13 Pro</a></div>
+        <div><a href="https://u.jd.com/uzHPi3F">在京东选购小米13 Pro</a></div>
         <br />
-        <div><a href="https://u.jd.com/Mz9L97u">在京东选购小米平板5</a></div>
+        <div><a href="https://u.jd.com/usH88LQ">在京东选购小米平板5</a></div>
         <br />
-        <div><a href="https://u.jd.com/Mb9z0vx">在京东选购Redmi Note 12 Pro</a></div>
+        <div><a href="https://u.jd.com/ubH0EQx">在京东选购Redmi Note 12 Turbo</a></div>
         <br />
-        <div><a href="https://u.jd.com/MqlzXwK">在京东选购Redmi K60</a></div>
+        <div><a href="https://u.jd.com/uuHYAfe">在京东选购Redmi Note 12 Pro</a></div>
         <br />
-        <div><a href="https://u.jd.com/Mil18ED">在京东选购Redmi K60 Pro</a></div>
+        <div><a href="https://u.jd.com/uzHvudF">在京东选购Redmi K60</a></div>
+        <br />
+        <div><a href="https://u.jd.com/u8HSxVT">在京东选购Redmi K60 Pro</a></div>
         <br />
       </div>
     </div>
