c9e9d33ced7d92fd94e88349e70faf2cfe74910f
Hege <hegeken@foxmail.com>
2023-04-21 16:05:58 +0800

    Ads Notice

--- components/EnAd.vue
+++ components/EnAd.vue
@@ -6,29 +6,27 @@
         <i class="mdui-panel-item-arrow mdui-icon material-icons">format_list_bulleted</i>
       </div>
       <div class="mdui-panel-item-body">
-        <div><b>May Safe for work Ads</b></div>
+        <div><b>Most of the Ads seems to be NOT SAFE for work!!!</b></div>
         <br />
-        <div><a href="https://www.highrevenuegate.com/g6jhi6vr?key=9b8c35729d9271b01b4e40e6f3ab6e6b">Ad No.1</a></div>
+        <div><a href="https://www.highrevenuegate.com/g6jhi6vr?key=9b8c35729d9271b01b4e40e6f3ab6e6b">Ad No.01</a></div>
         <br />
-        <div><a href="https://www.highrevenuegate.com/ic0gcn4dj?key=b16d256f37787c03fe6755c4a6580c78">Ad No.2</a></div>
+        <div><a href="https://www.highrevenuegate.com/ic0gcn4dj?key=b16d256f37787c03fe6755c4a6580c78">Ad No.02</a></div>
         <br />
-        <div><a href="https://www.highrevenuegate.com/zu8150zqbc?key=c0d5555762a8663cdd3621f2423b319f">Ad No.3</a></div>
+        <div><a href="https://www.highrevenuegate.com/zu8150zqbc?key=c0d5555762a8663cdd3621f2423b319f">Ad No.03</a></div>
         <br />
-        <div><a href="https://www.highrevenuegate.com/phqmf3fw?key=e7cb009e563fab1609919ce1db64732e">Ad No.4</a></div>
+        <div><a href="https://www.highrevenuegate.com/phqmf3fw?key=e7cb009e563fab1609919ce1db64732e">Ad No.04</a></div>
         <br />
-        <div><a href="https://www.highrevenuegate.com/kw42j4bywe?key=0f5d7fe735a644edc688c74f5a01ef97">Ad No.5</a></div>
+        <div><a href="https://www.highrevenuegate.com/kw42j4bywe?key=0f5d7fe735a644edc688c74f5a01ef97">Ad No.05</a></div>
         <br />
-        <div><b>Not Safe for Work Ads</b></div>
+        <div><a href="https://www.highrevenuegate.com/tntf7535y0?key=f4ddd5f01f266d8c4a2fa9a97a548d8d">Ad No.06</a></div>
         <br />
-        <div><a href="https://www.highrevenuegate.com/tntf7535y0?key=f4ddd5f01f266d8c4a2fa9a97a548d8d">Ad No.1</a></div>
+        <div><a href="https://www.highrevenuegate.com/nh9w4xmsnz?key=26c7af59f4c8e99575afe8102d370338">Ad No.07</a></div>
         <br />
-        <div><a href="https://www.highrevenuegate.com/nh9w4xmsnz?key=26c7af59f4c8e99575afe8102d370338">Ad No.2</a></div>
+        <div><a href="https://www.highrevenuegate.com/aahjpntzw?key=f7ae5d0044800ccc482fd3537c24c8de">Ad No.08</a></div>
         <br />
-        <div><a href="https://www.highrevenuegate.com/aahjpntzw?key=f7ae5d0044800ccc482fd3537c24c8de">Ad No.3</a></div>
+        <div><a href="https://www.highrevenuegate.com/bsk8mad1p?key=a790ead22044ceaeacf7024263ae8b4d">Ad No.09</a></div>
         <br />
-        <div><a href="https://www.highrevenuegate.com/bsk8mad1p?key=a790ead22044ceaeacf7024263ae8b4d">Ad No.4</a></div>
-        <br />
-        <div><a href="https://www.highrevenuegate.com/rncib4x39?key=5c57e5b543b79bc52ba6d2f625f53f09">Ad No.5</a></div>
+        <div><a href="https://www.highrevenuegate.com/rncib4x39?key=5c57e5b543b79bc52ba6d2f625f53f09">Ad No.10</a></div>
         <br />
       </div>
     </div>
